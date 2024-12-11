4ebcc7c951390229303bc2abd7164fd515f9860b
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-12-11 21:59:28 +0500

    Spring Boot init

---
+++ backend/src/test/java/com/blood/BloodDonation/BloodDonationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.blood.BloodDonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

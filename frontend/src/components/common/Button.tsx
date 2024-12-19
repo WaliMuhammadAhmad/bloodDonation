92ae8d7179c13367cdf6fa26939f74672f417725
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-12-19 14:17:49 +0500

    CSS updated

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -4,31 +4,13 @@ import { Link } from "react-router-dom";
 interface ButtonProps {
   children: React.ReactNode;
   to: string;
-  variant?: "filled" | "outlined"; // Optional variant prop
-  className?: string; // Optional class for additional styles
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  children,
-  to,
-  variant = "filled", // Default to "filled" if no variant is provided
-  className = "",
-}) => {
-  // Base styles for the button
-  const baseStyles = `text-center text-pretty px-6 py-3 rounded-lg border-2 transition-colors ${className}`;
-
-  // Styles for the "filled" variant
-  const filledStyles =
-    "bg-background text-text border-primary hover:bg-secondary";
-
-  // Styles for the "outlined" variant
-  const outlinedStyles =
-    "text-background border-background hover:bg-background hover:text-text";
-
-  // Combine base styles with variant-specific styles
-  const buttonStyles = `${baseStyles} ${
-    variant === "filled" ? filledStyles : outlinedStyles
-  }`;
+const Button: React.FC<ButtonProps> = ({ children, to, className = "" }) => {
+  const defaultStyles =
+    "w-full py-3 px-5 text-sm text-center font-medium border text-primary border-primary rounded-md hover:bg-primary hover:text-text focus:outline-none focus:ring-0";
+  const buttonStyles = `${defaultStyles} ${className}`;
 
   return (
     <Link to={to} className={buttonStyles}>
